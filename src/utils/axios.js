a69a660a68fac18a1a4c9d22c7995b6c5dff7c2b
vkharchenko <vkharchenko@vkharchenkosMBP.netis>
2020-12-27 00:46:23 +0200

    change baseURL to aws instance.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'http://ec2-18-219-251-82.us-east-2.compute.amazonaws.com/api'
 })
 
 export default axiosInstance
